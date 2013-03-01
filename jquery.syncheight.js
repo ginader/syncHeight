2bfbcac6e37616f3aaace27e5928e337fbb1a23d
Dirk Ginader <ginader@dhcp-172-19-8-79.mtv.corp.google.com>
2013-03-01 11:34:54 -0800

    v1.5 fixes issue with box-sizing: border-box
    fixes issue #2
    hat tip to https://github.com/bmsq for the hint

--- jquery.syncheight.js
+++ jquery.syncheight.js
@@ -12,15 +12,22 @@
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl.html
  *
- * Version: 1.4
+ * Version: 1.5
  *
  * Changelog
+ * * v1.5 fixes issue with box-sizing: border-box
  * * v1.4: new Method unSyncHeight() that removes previously added syncs i.e. for responsive layouts
  * * v1.3: compatibility fix for jQuery 1.9.x (removed $.browser)
  *
- * Usage:
+ * Usage sync:
 	$(window).load(function(){
 		$('p').syncHeight();
+	});
+ * Usage unsync: 
+	$(window).resize(function(){
+		if($(window).width() < 500){
+			$('p').unSyncHeight();
+		}
 	});
  */
 
@@ -55,7 +62,7 @@
 		$(selector).each(function() {
 			// fallback to auto height before height check ...
 			$(this).css(heightProperty.name, heightProperty.autoheightVal);
-			var val = $(this).height();
+			var val = parseInt($(this).css('height'),10);
 			if(val > max){
 				max = val;
 			}
